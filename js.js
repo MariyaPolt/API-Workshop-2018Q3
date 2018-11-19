99ef887fa4a7a5c26f960ef7178ac73de6fa23d7
Dzmitry <35468938+DrDiman@users.noreply.github.com>
2018-11-19 21:33:58 +0300

    add creation divs

--- js.js
+++ js.js
@@ -5,7 +5,12 @@ const urlForRequest = `${url}?q=${searchWords}&from=2018-11-19&sortBy=popularity
 
 function createContent(data){
   console.dir(data);
-  
+  for (let i = 0; i < data.articles.length; i++) {
+  const myDivPapa = document.createElement('div');
+  myDivPapa.classList.add('inputDataFromRequest');
+  myDivPapa.textContent = `${data.articles[i].description}`;
+  document.querySelector('article').appendChild(myDivPapa);
+}
 }
 
 function myRequest(url) {
@@ -13,9 +18,17 @@ function myRequest(url) {
   request.open('get', url);
   request.onload = () => {
     if (request.status === 200) {
-      const data = JSON.parse(request.response);
-      createContent(data);
+      const data = JSON.parse(request.response);      
+        createContent(data);
       }
   };
   request.send();
 }
+
+function myListener(e){
+  if (e.target===document.querySelector('.button')){
+    myRequest(urlForRequest);
+  }
+}
+
+document.body.addEventListener('click', myListener);
